5e73a40aa286695c7f8ca8485d5f7812d252c906
Carlos Queiroz <c.queiroz@cv-library.co.uk>
2024-11-08 11:12:36 +0000

    adding pushgateway, and send_metrics function

--- selenium/main.py
+++ selenium/main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import os
 #selenium stuff here
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -12,15 +12,34 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+USER = os.environ.get('USER', 'test')
+PASS = os.environ.get('PASS', 'test')
+
+
+def send_metric(job_name, metric_name, value, help_text, metric_type, pushgateway_url='http://localhost:9091'):
+    data = (
+        f"# HELP {metric_name} {help_text}\n"
+        f"# TYPE {metric_name} {metric_type}\n"
+        f"{metric_name} {value}\n"
+    )
+        
+    response = requests.post(
+        f"{pushgateway_url}/metrics/job/{job_name}",
+        data=data,
+        headers={'Content-Type': 'text/plain'}
+    )
+    
+    if response.status_code == 202 or response.status_code == 200 :
+        print("Metric sent successfully.")
+    else:
+        print(f"Failed to send metric: {response.status_code} {response.text}")
 
 
 
 loginURL = ('https://www.resume-library.com/hiring/login')
-
-
 keywords = 'IT'
-#keywords = 'bleblebleee' 
-
+#keywords = 'blablabla' 
+#keywords = 'blebleblee' 
 checkURL = (f'https://www.resume-library.com/client/resume-search/results?csrf_rl=6d31e39e4dfccf07bb50568b87d4c2e8&enc=&saved_search_id=&sort=&hidden-per-page=&advanced_search=0&search_builder=0&popular_search_id=&id=&recipients=&name=&frequency=daily&keywords={keywords}&keywords_exclude=%5B%22sales%22%5D&national=true&lat=&lon=&salary_from=&salary_to=&job_type%5B%5D=any&updated_on=4+months&education_level%5B%5D=any&work_experience%5B%5D=any&submt_btn=1')
 
 
@@ -39,18 +58,19 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 
 
 driver.get(loginURL)
-
+#time.sleep(1)
 # wait the page being loaded
 WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.NAME, 'user')))
 
-driver.find_element(By.NAME, 'user').send_keys('')
-driver.find_element(By.NAME, 'pass').send_keys('')
+driver.find_element(By.NAME, 'user').send_keys(USER)
+driver.find_element(By.NAME, 'pass').send_keys(PASS)
+time.sleep(1)
 driver.find_element(By.NAME, 'pass').send_keys(Keys.RETURN)
 
 
 
 driver.get(checkURL)
-
+#time.sleep(1)
 
 #WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.NAME, 'resume-search-displaying-left')))
 
@@ -66,12 +86,24 @@ if result_container:
     if 'Displaying 0 Resumes matched' in matched_number:
         print("error")
         print(matched_number)
+        result = int_value = int(''.join(filter(str.isdigit, matched_number)))
     else:
         print("passed")
         print(matched_number)
+        result = int_value = int(''.join(filter(str.isdigit, matched_number)))
+
+print(result)
 
 
 driver.save_screenshot('screenshot.png')
-
+#time.sleep(1)
 driver.quit()
 
+
+send_metric(
+    job_name='synthetix',
+    metric_name='synthetix_rl_hiring_results',
+    value=result,
+    help_text='Synthetix check to gather the results on rl/hiring',
+    metric_type='gauge'
+)
